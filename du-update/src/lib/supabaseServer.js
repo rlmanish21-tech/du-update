431e23da3a83fdf8d81df4859c753e3c6d8b9b8b
rlmanish21-tech <rlmanish21@gmail.com>
2026-08-15 18:36:06 +0530

    Refactor createClient to use nullish coalescing

--- du-update/src/lib/supabaseServer.js
+++ du-update/src/lib/supabaseServer.js
@@ -3,19 +3,13 @@ import { cookies } from 'next/headers'
 
 export function createClient() {
   const cookieStore = cookies()
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    {
-      cookies: {
-        get(name)         { return cookieStore.get(name)?.value },
-        set(name, value, options) {
-          try { cookieStore.set({ name, value, ...options }) } catch {}
-        },
-        remove(name, options) {
-          try { cookieStore.set({ name, value: '', ...options }) } catch {}
-        },
-      },
-    }
-  )
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL ?? ''
+  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ''
+  return createServerClient(url, key, {
+    cookies: {
+      get(name)             { return cookieStore.get(name)?.value },
+      set(name, value, opt) { try { cookieStore.set({ name, value, ...opt }) } catch {} },
+      remove(name, opt)     { try { cookieStore.set({ name, value: '', ...opt }) } catch {} },
+    },
+  })
 }
