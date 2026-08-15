cc388afb6337bfe86ba7dfda6b00fc20e974ad9c
rlmanish21-tech <rlmanish21@gmail.com>
2026-08-15 23:01:24 +0530

    Fix cookieStore initialization in createClient function

--- du-update/src/lib/supabaseServer.js
+++ du-update/src/lib/supabaseServer.js
@@ -2,9 +2,9 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export function createClient() {
-  const cookieStore = cookies()
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL ?? ''
   const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ''
+  const cookieStore = cookies()
   return createServerClient(url, key, {
     cookies: {
       get(name)             { return cookieStore.get(name)?.value },
